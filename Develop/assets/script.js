abc27fc0068764108155087e35c334fcb58a6667
Tobias Virgona <tobiasvirgona@gmail.com>
2022-08-31 20:47:44 +1000

    had help getting my items to store on local. needed to index and then call the index. Now entered my function that pulls from the storage

--- Develop/assets/script.js
+++ Develop/assets/script.js
@@ -30,13 +30,25 @@ $('#4pm').text(fourPm);
 var fivePm = moment("05:00:00 PM", "h:mm:ss A").format('ha');
 $('#5pm').text(fivePm);
 
+function getList() {
+    // pulls items from the local storage to stay in the planner when refreshed
+    $("#9 .description").val(localStorage.getItem("9"));
+    $("#10 .description").val(localStorage.getItem("10"));
+    $("#11 .description").val(localStorage.getItem("11"));
+    $("#12 .description").val(localStorage.getItem("12"));
+    $("#1 .description").val(localStorage.getItem("1"));
+    $("#2 .description").val(localStorage.getItem("2"));
+    $("#3 .description").val(localStorage.getItem("3"));
+    $("#4 .description").val(localStorage.getItem("4"));
+    $("#5 .description").val(localStorage.getItem("5"));
+}
 
 // storing entered todo's into local storage
 $('.saveBtn').on("click", function () {
 
     console.log(this);
     var text = $(this).siblings(".description").val();
-    var time = $('.time');
+    var time = $(this).attr("index");
 
     localStorage.setItem(time, text);
 })
@@ -52,5 +64,5 @@ $('.saveBtn').on("click", function () {
 //         console.log(timeOnTracker, currentTime);
 //     })
 // }
-
+getList()
 // timeTracker()
